2aecac7099baf28abe381b8201b7601ba3c9cfd4
Pasindu03 <pasindu.yapa03@gmail.com>
2024-08-15 13:31:50 +0530

    Customer Finished

--- src/main/java/com/example/posb/persistance/Customer.java
+++ src/main/java/com/example/posb/persistance/Customer.java
@@ -1,12 +1,14 @@
 package com.example.posb.persistance;
 
 import com.example.posb.dto.CustomerDto;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public sealed interface Customer permits CustomerProcess {
-    String saveCustomer(CustomerDto dto, Connection connection);
+    String saveCustomer(CustomerDto dto, Connection connection) throws SQLException;
+    String deleteCustomer(String id, Connection connection) throws SQLException;
+    boolean updateCustomer(String id, CustomerDto dto, Connection connection) throws SQLException;
+    CustomerDto getCustomer(String id, Connection connection) throws SQLException;
+    String getAllCustomer(CustomerDto dto, Connection connection) throws SQLException;
 }
